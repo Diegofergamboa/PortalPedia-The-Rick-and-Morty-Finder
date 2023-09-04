3dc3db627e47b8a3fc0cdc8b7684a5a8b8cbf133
diegofergamboa <diegofergamboa@hotmail.es>
2023-09-04 17:25:00 -0500

    Setup from the API finished

--- src/hooks/useApi.jsx
+++ src/hooks/useApi.jsx
@@ -1,4 +1,39 @@
 import React from "react";
 
 
-const useApi(api)
+const useApi = () => {
+    const [data, setData] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState('')
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true)
+            const urlApi = 'https://rickandmortyapi.com/api/character'
+            try {
+                const response = await fetch(`${urlApi}`)
+                const result = await response.json()
+                return (response.ok) && setData(result)
+            } catch (error) {
+                setError(error)
+                console.log(`Error fetching the data with ${error}`)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchData()
+    }, [])
+
+    return {
+        data,
+        setData,
+        error,
+        setError,
+        loading,
+        setLoading
+    }
+
+}
+
+export { useApi }
+
